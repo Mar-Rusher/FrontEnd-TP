0135a0b4090f66861482b3f3c74ac6cbc93967e2
Isa Mesina <isabelmesina@gmail.com>
2024-05-09 18:50:07 -0300

    primer mergeada

---
+++ validation-register.js
@@ -0,0 +1,23 @@
+let btnSend = document.querySelector("#btn-send");
+
+btnSend.addEventListener('click', function(){
+    let firstname = document.querySelector("#firstname");
+    let lastname = document.querySelector("#lastname");
+    let email = document.querySelector("#email");
+    
+    if(firstname.value.trim()==='' && firstname.required ){
+        let errorFirstname = document.querySelector("#error-firstname");
+        errorFirstname.textContent='Debe completar el campo nombre';
+        // alert('Error, debe completar el campo nombre');
+    }
+
+    if(!lastname.value.trim() && lastname.required ){
+        document.querySelector("#error-lastname").textContent = 'Debe completar el campo apellido';
+        // alert('Error, debe completar el campo apellido');
+    }
+
+    if(!email.value.trim() && email.required ){
+        document.querySelector("#error-email").textContent = 'Debe completar el campo email';
+        // alert('Error, debe completar el campo email');
+    }
+});
